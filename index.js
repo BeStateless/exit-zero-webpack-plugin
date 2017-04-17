a0fc0662abdb16bed44b4854080f34e04bebf495
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-04-16 19:25:42 -0600

    remove error display; add activation function option

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function ExitZeroWebpackPlugin (options) {
   this.active = options.active == null ? true : options.active;
+  this.activationFunction = options.activationFunction == null ? () => true : options.activationFunction;
 };
 
 ExitZeroWebpackPlugin.prototype.apply = function (compiler) {
   compiler.plugin('done', (stats, callback) => {
     if (stats.compilation.errors && stats.compilation.errors.length) {
-      console.log('There were ' + stats.compilation.errors.length - 1 + ' errors.');
       if (this.active) {
         process.exit(0);
       }
